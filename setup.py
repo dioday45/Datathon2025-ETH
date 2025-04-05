96547d9986da69fa20dcd7ff80919db5161774ea
Jeremy Di Dio <65977803+dioday45@users.noreply.github.com>
2025-04-05 13:32:35 +0200

    environment setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="quaintly",
+    version="0.1.0",
+    description="Alpiq Datathon 2025",
+    author="QuAIntly",
+    packages=find_packages(),
+    python_requires=">=3.8",
+)
