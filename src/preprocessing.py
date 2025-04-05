3ac259223218f9f696b3f80268721bbc88bd2684
Jeremy Di Dio <65977803+dioday45@users.noreply.github.com>
2025-04-06 00:38:37 +0200

    preprocessing cleaning

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+import holidays
 import pandas as pd
 
 
@@ -19,16 +20,21 @@ class PreProcessClass(ABC):
         if id not in self.x.columns:
             raise ValueError(f"Customer ID '{id}' not found in the dataset.")
 
-        def drop_before(ts: pd.Series):
-            first_nan = ts.first_valid_index()
-            return ts[first_nan:]
+        if "ES" in id:
+            # Use Spanish holidays
+            country_holidays = holidays.country_holidays("ES")
+        elif "IT" in id:
+            # Use Italian holidays
+            country_holidays = holidays.country_holidays("IT")
+        else:
+            # Default to Spanish holidays if the country is not recognized
+            raise ValueError(f"Country not recognized for ID '{id}'")
 
         customer_ts = self.x[[id, "spv", "temp"]]
-        customer_ts = drop_before(customer_ts)
         customer_ts = customer_ts.rename(columns={id: "Consumption"})
-
-        if not pd.api.types.is_datetime64_any_dtype(customer_ts.index):
-            customer_ts.index = pd.to_datetime(customer_ts.index)
+        first_idx = customer_ts["Consumption"].first_valid_index()
+        customer_ts = customer_ts.loc[first_idx:]
+        customer_ts.index = pd.to_datetime(customer_ts.index)
 
         # Add additional features
         customer_ts["Hour"] = customer_ts.index.hour.astype(int)
@@ -36,28 +42,33 @@ class PreProcessClass(ABC):
         customer_ts["Month"] = customer_ts.index.month.astype(int)
         customer_ts["Year"] = customer_ts.index.year.astype(int)
         customer_ts["Dow"] = customer_ts.index.day_of_week.astype(int)
-        customer_ts["DayYear"] = customer_ts.index.dayofyear.astype(int)
-        customer_ts["Week"] = customer_ts.index.isocalendar().week.astype(int)
-        customer_ts["Season"] = customer_ts.index.quarter.astype(int)
         customer_ts["IsWeekend"] = (customer_ts.index.weekday >= 5).astype(int)
+        customer_ts["is_holiday"] = [
+            int(d in country_holidays) for d in customer_ts.index.normalize().date
+        ]
 
-        # Create special_weekend feature
-        customer_ts["IsWeekendSpecial"] = 0
-        customer_ts.loc[
-            (customer_ts["Dow"] == 5) & (customer_ts["Hour"] >= 20),
-            "IsWeekendSpecial",
-        ] = 1
-        customer_ts.loc[(customer_ts["Dow"] == 6), "IsWeekendSpecial"] = 1
-        customer_ts.loc[
-            (customer_ts["Dow"] == 0) & (customer_ts["Hour"] <= 6),
-            "IsWeekendSpecial",
-        ] = 1
+        # # Create special_weekend feature
+        # customer_ts["IsWeekendSpecial"] = 0
+        # customer_ts.loc[
+        #     (customer_ts["Dow"] == 5) & (customer_ts["Hour"] >= 20),
+        #     "IsWeekendSpecial",
+        # ] = 1
+        # customer_ts.loc[(customer_ts["Dow"] == 6), "IsWeekendSpecial"] = 1
+        # customer_ts.loc[
+        #     (customer_ts["Dow"] == 0) & (customer_ts["Hour"] <= 6),
+        #     "IsWeekendSpecial",
+        # ] = 1
 
         # Create active_day feature
         customer_ts["ActiveDay"] = 0
         customer_ts.loc[
             (customer_ts["Hour"] >= 6) & (customer_ts["Hour"] <= 20), "ActiveDay"
         ] = 1
+
+        customer_ts["Consumption"] = customer_ts["Consumption"].clip(
+            lower=customer_ts["Consumption"].quantile(0.01),
+            upper=customer_ts["Consumption"].quantile(0.99),
+        )
 
         return customer_ts
 
