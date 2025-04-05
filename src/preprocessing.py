43a57a68d3f40228af81bf4eeac2df7ae681cff3
Jeremy Di Dio <65977803+dioday45@users.noreply.github.com>
2025-04-05 21:23:16 +0200

    update preprocess

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,6 @@
-from abc import ABC, abstractmethod
-from src.data import DataLoader
+from abc import ABC
 
-from os.path import join
-
-import numpy as np
 import pandas as pd
-
-from src.utils import *
 
 
 class PreProcessClass(ABC):
@@ -29,73 +23,41 @@ class PreProcessClass(ABC):
             first_nan = ts.first_valid_index()
             return ts[first_nan:]
 
-        def find_nan_streaks(ts: pd.Series):
-            nan_streak_starts = ts.isna() & ~ts.isna().shift(fill_value=False)
-            nan_streak_ends = ts.isna() & ~ts.isna().shift(-1, fill_value=False)
-            first_nan_indices = ts.index[nan_streak_starts]
-            last_nan_indices = ts.index[nan_streak_ends]
-
-            return last_nan_indices - first_nan_indices, first_nan_indices
-
-        customer_ts = self.x[[id, "spv", "temp"]].dropna(subset=[id])
+        customer_ts = self.x[[id, "spv", "temp"]]
         customer_ts = drop_before(customer_ts)
-        length_nan, start_nan = find_nan_streaks(customer_ts[id])
-        print(length_nan)
-        print(start_nan)
         customer_ts = customer_ts.rename(columns={id: "Consumption"})
 
         if not pd.api.types.is_datetime64_any_dtype(customer_ts.index):
             customer_ts.index = pd.to_datetime(customer_ts.index)
 
         # Add additional features
-        customer_ts["Hour"] = customer_ts.index.hour
-        customer_ts["Day"] = customer_ts.index.day
-        customer_ts["Month"] = customer_ts.index.month
-        customer_ts["Year"] = customer_ts.index.year
-        customer_ts["Dow"] = customer_ts.index.day_name()
-        customer_ts["DayYear"] = customer_ts.index.dayofyear
-        customer_ts["Week"] = customer_ts.index.isocalendar().week
-        customer_ts["Season"] = customer_ts.index.quarter
-        customer_ts["IsWeekend"] = customer_ts.index.weekday >= 5
+        customer_ts["Hour"] = customer_ts.index.hour.astype(int)
+        customer_ts["Day"] = customer_ts.index.day.astype(int)
+        customer_ts["Month"] = customer_ts.index.month.astype(int)
+        customer_ts["Year"] = customer_ts.index.year.astype(int)
+        customer_ts["Dow"] = customer_ts.index.day_of_week.astype(int)
+        customer_ts["DayYear"] = customer_ts.index.dayofyear.astype(int)
+        customer_ts["Week"] = customer_ts.index.isocalendar().week.astype(int)
+        customer_ts["Season"] = customer_ts.index.quarter.astype(int)
+        customer_ts["IsWeekend"] = (customer_ts.index.weekday >= 5).astype(int)
 
         # Create special_weekend feature
-        customer_ts["IsWeekendSpecial"] = False
+        customer_ts["IsWeekendSpecial"] = 0
         customer_ts.loc[
-            (customer_ts["Dow"] == "Saturday") & (customer_ts["Hour"] >= 20),
+            (customer_ts["Dow"] == 5) & (customer_ts["Hour"] >= 20),
             "IsWeekendSpecial",
-        ] = True
-        customer_ts.loc[(customer_ts["Dow"] == "Sunday"), "IsWeekendSpecial"] = True
+        ] = 1
+        customer_ts.loc[(customer_ts["Dow"] == 6), "IsWeekendSpecial"] = 1
         customer_ts.loc[
-            (customer_ts["Dow"] == "Monday") & (customer_ts["Hour"] <= 6),
+            (customer_ts["Dow"] == 0) & (customer_ts["Hour"] <= 6),
             "IsWeekendSpecial",
-        ] = True
+        ] = 1
 
         # Create active_day feature
-        customer_ts["ActiveDay"] = False
+        customer_ts["ActiveDay"] = 0
         customer_ts.loc[
             (customer_ts["Hour"] >= 6) & (customer_ts["Hour"] <= 20), "ActiveDay"
-        ] = True
-
-        # Rename season
-        season_map = {1: "Winter", 2: "Spring", 3: "Summer", 4: "Autumn"}
-
-        customer_ts["Season"] = customer_ts["Season"].map(season_map)
-
-        list_to_cat = [
-            "Hour",
-            "Day",
-            "Month",
-            "Year",
-            "Dow",
-            "DayYear",
-            "Week",
-            "Season",
-        ]
-
-        list_to_bool = ["IsWeekendSpecial", "ActiveDay"]
-
-        customer_ts[list_to_cat] = customer_ts[list_to_cat].astype("category")
-        customer_ts[list_to_bool] = customer_ts[list_to_bool].astype("bool")
+        ] = 1
 
         return customer_ts
 
