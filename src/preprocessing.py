c629e796a140b4d6fc07d45bfe9a46d1207af3c8
martint201 <martint.leclercq@gmail.com>
2025-04-05 18:41:36 +0200

    nan drop

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,13 +5,23 @@ import pandas as pd
 
 
 class PreProcessClass(ABC):
-    def __init__(self):
-        pass
+    def __init__(self, x: pd.DataFrame):
+        self.x = x
+
+    def preprocess_nonan(self, id: pd.DataFrame) -> pd.DataFrame:
+        """data cleaning + imputation + standardization etc."""
+        # fill na
+        customer_ts = self.x[id].dropna()
+
+        return customer_ts
 
     def preprocess(self, x: pd.DataFrame) -> pd.DataFrame:
         """data cleaning + imputation + standardization etc."""
+        # fill na
+
+        # features
         pass
 
-    @abstractmethod
-    def feature_engineering(self, x: pd.DataFrame) -> pd.DataFrame:
-        pass
+    # @abstractmethod
+    # def feature_engineering(self, x: pd.DataFrame) -> pd.DataFrame:
+    #     pass
